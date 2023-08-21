7a7ecb7b2e32d6a6a93e3c7afd9b690cac26ac5f
liskovich <ricardsliskovskis@gmail.com>
2023-08-21 11:53:31 +0300

    Minor fixes

--- ricai_unittestgen_toolkit.py
+++ ricai_unittestgen_toolkit.py
@@ -2,20 +2,14 @@ from abc import ABC
 from typing import List
 
 from superagi.tools.base_tool import BaseToolkit, BaseTool
-from ricai_unittestgen_generate_tool import RicaiCodestoreUpdateTool
+from ricai_unittestgen_generate_tool import RicaiUnitTestGenGenerateTool
 
 class RicaiUnitTestGenToolkit(BaseToolkit, ABC):
     name: str = "RicAI Unit Test Gen Toolkit"
     description: str = "Toolkit containing tools for unit test generation based on provided code and software requirement context"
 
     def get_tools(self) -> List[BaseTool]:
-        return [RicaiCodestoreUpdateTool()]
+        return [RicaiUnitTestGenGenerateTool()]
 
     def get_env_keys(self) -> List[str]:
-        return [
-            "WEAVIATE_URL",
-            "WEAVIATE_API_KEY",
-            "OPENAI_API_KEY",
-            "GITHUB_USERNAME",
-            "GITHUB_ACCESS_TOKEN",
-            ]
+        return ["OPENAI_API_KEY"]
